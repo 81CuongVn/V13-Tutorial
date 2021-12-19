d4cc782ed48e5bf1a30f51a85f215e0bb9cef837
ViperCod <86508744+ViperCod@users.noreply.github.com>
2021-12-18 23:45:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js')
 const config = require('./config.json')
 const loadCommands = require('./Commands/load-commands')
+const loadFeatures = require('./Features/load-features')
 
 const mongo = require('./util/mongo')
 
@@ -11,17 +12,39 @@ const client = new Discord.Client({
     ]
 })
 
-client.on('ready', async() => {
+client.commands = new Discord.Collection()
+client.aliases = new Discord.Collection()
+
+client.on('ready', async () => {
     console.log('The bot is ready!')
 
     await mongo().then(mongoose => {
-        try{
+        try {
             console.log('Connected to mongo!')
-        }finally{
+        } finally {
         }
     })
 
     loadCommands(client)
+    loadFeatures(client)
 })
+
+client.on('messageCreate', (message) => {
+    const prefix = config.prefix
+    if(!message.content.startsWith(prefix)) return
+    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+    const command = args.shift().toLowerCase()
+    const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command))
+    if(!cmd)return
+    if (cmd.inVoiceChannel && !message.member.voice.channel) return message.channel.send('You must be in a voice channel!')
+    try{
+        cmd.run(client, message, args)
+    }catch (e){
+        console.error(e)
+        message.reply(`Error: ${e}`)
+    }
+})
+
+module.exports = client
 
 client.login(config.token)
